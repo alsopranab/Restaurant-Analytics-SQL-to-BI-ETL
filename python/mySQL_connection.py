765708d5502d5565840036d8954cdaaf13b60d6b
Pranab Debnath <62995713+alsopranab@users.noreply.github.com>
2026-01-07 22:05:29 +0530

    Update MySQL connection script
    Added connection confirmation and removed sanity check.

---
+++ python/mySQL_connection.py
@@ -0,0 +1,16 @@
+import mysql.connector # we use this to connect with the sql
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="analytics_user",
+    password="Analytics@123",
+    database="restaurant_db",
+    port=3306
+)
+
+cursor = conn.cursor() # we use this to execuate manually
+
+print("Connected to MySQL")
